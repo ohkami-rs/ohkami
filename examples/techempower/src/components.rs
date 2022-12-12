a18d821ea9c2edb2eeec91813a0855ce2b967c31
kana-rus <kanarus786@gmail.com>
2022-12-13 01:37:05 +0900

    Fix: examples/techempower

--- examples/techempower/src/components.rs
+++ examples/techempower/src/components.rs
@@ -1,58 +1,54 @@
-use std::fmt::Write;
-use ohkami::prelude::Body;
-use serde::Serialize;
-use sqlx::FromRow;
+pub(crate) mod consts {
+    use std::ops::RangeInclusive;
 
+    pub const RAND_RANGE: RangeInclusive<usize>  = 1..=10000;
 
-#[derive(FromRow, Serialize)]
-pub(crate) struct WorldRow {
-    id:            i32,
-    random_number: i32,
+    pub const PREPARE_GET_WORLD:    &'static str = "SELECT id, randomnumber FROM world WHERE id = $1";
+    pub const PREPARE_GET_FORTUNE:  &'static str = "SELECT id, message FROM fortune";
+    pub const PREPARE_UPDATE_WORLD: &'static str = "UPDATE world SET randomnumber = $1 WHERE id = $2 RETURNUNG id, randomnumber";
+
+    pub const DB_URL:              &'static str  = "postgres://benchmarkdbuser:benchmarkdbpass@tfb-database/hello_world?sslmode=disable";
+    pub const MAX_CONNECTIONS:     u32           = 10000;
 }
 
-#[derive(FromRow, Serialize)]
-pub(crate) struct Fortune {
-    pub id:     i32,
-    pub message: String
-}
-pub(crate) fn html_from(mut fortunes: Vec<Fortune>) -> Body {
-    fortunes.sort_unstable_by(|it, next|
-        it.message.cmp(&next.message)
-    );
+pub(crate) mod models {
+    use serde::Serialize;
+    use sqlx::FromRow;
 
-    Body::text_html(fortunes
-        .into_iter()
-        .fold(
-            String::from("<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>"),
-            |it, next| it + &format!("<tr><td>{}</td><td>{}</td></tr>", next.id, next.message)
-        ) + "</table></body></html>"
-    )
+    #[derive(FromRow, Serialize)]
+    pub struct World {
+        id:           i32,
+        randomnumber: i32,
+    }
+
+    #[derive(FromRow, Serialize)]
+    pub struct Fortune {
+        pub id:     i32,
+        pub message: String
+    }
 }
 
-pub(crate) fn random_i32() -> i32 {
-    (rand::random::<u32>() % 10000 + 1) as i32
+pub(crate) mod functions {
+    use ohkami::response::Body;
+    use rand::{Rng, rngs::ThreadRng};
+    use super::{models::Fortune, consts::RAND_RANGE};
+
+    pub fn html_from(fortunes: Vec<Fortune>) -> Body {
+        Body::text_html(fortunes
+            .into_iter()
+            .fold(
+                String::from("<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>"),
+                |it, next| it + &format!("<tr><td>{}</td><td>{}</td></tr>", next.id, next.message)
+            ) + "</table></body></html>"
+        )
+    }
+
+    pub fn random_i32(generator: &mut ThreadRng) -> i32 {
+        generator.gen_range(RAND_RANGE) as i32
+    }
 }
 
-pub(crate) const PREPARE_GET_WORLD:   &'static str = "SELECT id, randomnumber FROM world WHERE id = $1";
-pub(crate) const PREPARE_GET_FORTUNE: &'static str = "SELECT id, message FROM fortune";
-#[allow(non_snake_case)]
-pub(crate) fn PREPARE_UPDATE_WORLDS(random_number: i32) -> String {
-    // let mut updates = Vec::with_capacity(500);
-    // for num in 1..=500_u16 {
-        let (mut query, mut pl) = (String::with_capacity(73 + 27*random_number as usize), 1_u16);
-        query.push_str("UPDATE world SET randomnumber = CASE id ");
-        for _ in 1..=random_number {
-            let _ = write!(&mut query, "when ${} then ${} ", pl, pl + 1);
-            pl += 2
-        }
-        query.push_str("ELSE randomnumber END WHERE id IN (");
-        for _ in 1..=random_number {
-            let _ = write!(&mut query, "${},", pl);
-            pl += 1
-        }
-        query.pop();
-        query.push(')');
 
-        query.shrink_to_fit();
-        query
-}
+
+
+
--- examples/techempower/src/main.rs
+++ examples/techempower/src/main.rs
@@ -1,9 +1,14 @@
 mod components; use components::{
-    WorldRow, Fortune, html_from,
-    random_i32,
-    PREPARE_GET_WORLD, PREPARE_GET_FORTUNE, PREPARE_UPDATE_WORLDS, 
+    consts::{
+        DB_URL, PREPARE_GET_WORLD, PREPARE_GET_FORTUNE, PREPARE_UPDATE_WORLD, MAX_CONNECTIONS,
+    },
+    models::{
+        World, Fortune,
+    },
+    functions::{
+        random_i32, html_from,
+    },
 };
-
 use ohkami::{prelude::*, json};
 use sqlx::postgres::PgPoolOptions;
 
@@ -11,8 +16,8 @@ use sqlx::postgres::PgPoolOptions;
 fn main() -> Result<()> {
     let config = Config {
         db_profile: DBprofile {
-            pool_options: PgPoolOptions::new().max_connections(10000),
-            url:          "postgres://benchmarkdbuser:benchmarkdbpass@tfb-database/ohkami",
+            pool_options: PgPoolOptions::new().max_connections(MAX_CONNECTIONS),
+            url:          DB_URL,
         },
         ..Default::default()
     };
@@ -28,8 +33,8 @@ fn main() -> Result<()> {
 }
 
 async fn get_db(ctx: Context) -> Result<Response> {
-    let id = random_i32();
-    let world = sqlx::query_as::<_, WorldRow>(PREPARE_GET_WORLD)
+    let id = random_i32(&mut rand::thread_rng());
+    let world = sqlx::query_as::<_, World>(PREPARE_GET_WORLD)
         .bind(id)
         .fetch_one(ctx.pool())
         .await?;
@@ -44,18 +49,53 @@ async fn get_fortunes(ctx: Context) -> Result<Response> {
         id:      0,
         message: "Additional fortune added at request time.".into(),
     });
+    fortunes.sort_unstable_by(|it, next| it.message.cmp(&next.message));
 
     Response::OK(html_from(fortunes))
 }
 
 async fn get_queries(ctx: Context) -> Result<Response> {
-    let q = ctx.query("q").unwrap().parse::<u32>().unwrap();
-    // let worlds = 
-    todo!()
+    let count = {
+        let queries = ctx.query("queries").unwrap_or("1").parse::<u32>().unwrap_or(1);
+        if queries < 1 {1} else if 500 < queries {500} else {queries}
+    } as usize;
+
+    let mut worlds = Vec::with_capacity(count);
+    //let mut generator = rand::thread_rng();////////////////////////////////// <-------------- NOT `Send`
+
+    for _ in 0..count {
+        let random_id = 1;//random_i32(&mut generator);
+        worlds.push(
+            sqlx::query_as::<_, World>(PREPARE_GET_WORLD)
+                .bind(random_id)
+                .fetch_one(ctx.pool())
+                .await?
+        )
+    }
+
+    Response::OK(JSON::from_struct(&worlds)?)
 }
 
 async fn get_updates(ctx: Context) -> Result<Response> {
-    let q = ctx.query("q").unwrap().parse::<u32>().unwrap();
-    todo!()
+    let count = {
+        let queries = ctx.query("queries").unwrap_or("1").parse::<u32>().unwrap_or(1);
+        if queries < 1 {1} else if 500 < queries {500} else {queries}
+    } as usize;
 
+    let mut worlds = Vec::with_capacity(count);
+    //let mut generator = rand::thread_rng();////////////////////////////////// <-------------- NOT `Send`
+
+    for _ in 0..count {
+        let random_id = 1;//random_i32(&mut generator);
+        let new_random_number = 1;//random_i32(&mut generator);
+        worlds.push(
+            sqlx::query_as::<_, World>(PREPARE_UPDATE_WORLD)
+                .bind(new_random_number)
+                .bind(random_id)
+                .fetch_one(ctx.pool())
+                .await?
+        )
+    }
+
+    Response::OK(JSON::from_struct(&worlds)?)
 }
