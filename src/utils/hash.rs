f8261fdfc4231258a961f71cd471085f5eebd29f
kana-rus <kanarus786@gmail.com>
2022-12-14 00:16:59 +0900

    Fix: hash

--- src/utils/hash.rs
+++ src/utils/hash.rs
@@ -3,22 +3,23 @@ use crate::{
     prelude::Response,
 };
 
-const BIG_PRIME:       usize = 124901413;
-const HASH_TABLE_SIZE: usize = 97;
+const HASH_SIZE: usize = 1000000009;
+const PRIME:     usize = 29;
 
+const fn alphabet_index(ch: &u8) -> usize {(*ch - b'a') as usize}
 fn hash(key: &str) -> usize {
-    key.as_bytes().into_iter().fold(0, |current_hash, ch|
-        (current_hash * BIG_PRIME + *ch as usize) % HASH_TABLE_SIZE
+    key.as_bytes().into_iter().rfold(0, |hash, ch|
+        (hash * PRIME + alphabet_index(ch)) % HASH_SIZE
     )
 }
 
 pub(crate) struct StringHashMap(
-    [Option<String>; HASH_TABLE_SIZE]
+    [Option<String>; HASH_SIZE]
 ); impl StringHashMap {
     pub fn new() -> Result<Self> {
         Ok(Self(
-            TryInto::<[Option<String>; HASH_TABLE_SIZE]>::try_into(
-                std::vec::from_elem(None, HASH_TABLE_SIZE)
+            TryInto::<[Option<String>; HASH_SIZE]>::try_into(
+                std::vec::from_elem(None, HASH_SIZE)
             )._else(|_| Response::InternalServerError("Failed in type casting"))?
         ))
     }
