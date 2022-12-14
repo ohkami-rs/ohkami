22bdb49d8291a39df4b4998022762952cd342c9c
kana-rus <kanarus786@gmail.com>
2022-12-14 17:53:23 +0900

    Change: hash algorithm: linear congruential

--- src/utils/hash.rs
+++ src/utils/hash.rs
@@ -3,18 +3,18 @@ use crate::{
     prelude::Response,
 };
 
-const HASH_SIZE: usize = 1000000009;
-const PRIME:     usize = 29;
-const fn alphabet_index(alphabet: &u8) -> usize {
-    (*alphabet - b'a') as usize
-}
-fn polynomial_rolling_hash(key: &str) -> usize {
-    key.as_bytes()
-        .into_iter()
-        .rfold(0, |hash, byte|
-            (hash * PRIME + alphabet_index(byte)) % HASH_SIZE
-        )
-}
+// const HASH_SIZE: usize = 20000063;
+// const PRIME:     usize = 29;
+// const fn alphabet_index(alphabet: &u8) -> usize {
+//     (*alphabet - b'a') as usize
+// }
+// fn polynomial_rolling_hash(key: &str) -> usize {
+//     key.as_bytes()
+//         .into_iter()
+//         .rfold(0, |hash, byte|
+//             (hash * PRIME + alphabet_index(byte)) % HASH_SIZE
+//         )
+// }
 
 // fn fnv_1_hash_32bit(key: &str) -> u32 {
 //     key.as_bytes()
@@ -24,23 +24,33 @@ fn polynomial_rolling_hash(key: &str) -> usize {
 //         )
 // }
 
+const BIG_PRIME:  usize = 1212121;
+const TABLE_SIZE: usize = 2357;
+fn linear_congruential_hash(key: &str) -> usize {
+    key.as_bytes()
+        .into_iter()
+        .fold(0, |hash, byte|
+            (hash * BIG_PRIME + *byte as usize) % TABLE_SIZE
+        )
+}
+
 pub(crate) struct StringHashMap(
-    [Option<String>; HASH_SIZE]
+    [Option<String>; TABLE_SIZE]
 ); impl StringHashMap {
     pub fn new() -> Result<Self> {
         Ok(Self(
-            TryInto::<[Option<String>; HASH_SIZE]>::try_into(
-                std::vec::from_elem(None, HASH_SIZE)
+            TryInto::<[Option<String>; TABLE_SIZE]>::try_into(
+                std::vec::from_elem(None, TABLE_SIZE)
             )._else(|_| Response::InternalServerError("Failed in type casting"))?
         ))
     }
     pub fn get(&self, key: &str) -> Option<&str> {
         self.0.as_ref()
-            .get(polynomial_rolling_hash(key))?
+            .get(linear_congruential_hash(key) as usize)?
             .as_ref()
             .map(|string| &**string)
     }
     pub fn insert(&mut self, key: &str, value: String) {
-        self.0[polynomial_rolling_hash(key)] = Some(value)
+        self.0[linear_congruential_hash(key) as usize] = Some(value)
     }
 }
