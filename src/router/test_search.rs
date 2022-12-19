1fb78eafe09538a0750b4879df8012f0f81e4f00
kana-rus <kanarus786@gmail.com>
2022-12-19 23:04:39 +0900

    NW; 2022-12-19 23:04

--- src/router/test_search.rs
+++ src/router/test_search.rs
@@ -1,8 +1,18 @@
 #![cfg(test)]
 #![allow(unused)]
 
+use super::Router;
+use crate::test_system::Method::*;
 
 #[test]
 fn search_one_1() {
-    
+    let mock_handler = 100;
+
+    let mut router = Router::new();
+    router.register(GET, "/", mock_handler);
+
+    assert_eq!(
+        router.search(GET, "/"),
+        Some(&mock_handler)
+    )
 }
