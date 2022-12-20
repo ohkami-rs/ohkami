c4f2d9d2b00e8e80e80071cd408e004d60c7c10c
kana-rus <kanarus786@gmail.com>
2022-12-20 15:54:44 +0900

    Add: search tests

--- src/router/test_search.rs
+++ src/router/test_search.rs
@@ -16,3 +16,86 @@ fn search_one_1() {
         Some(&mock_handler)
     )
 }
+#[test]
+fn search_one_2() {
+    let mock_handler = 100;
+
+    let mut router = Router::new();
+    router.register(GET, "/api", mock_handler);
+
+    assert_eq!(
+        router.search(GET, "/api"),
+        Some(&mock_handler)
+    )
+}
+
+#[test]
+fn search_two_pararel_1() {
+    let (mock_handler_1, mock_handler_2) = (100, 200);
+
+    let mut router = Router::new();
+    router.register(GET, "/api", mock_handler_1);
+    router.register(POST, "/api", mock_handler_2);
+
+    assert_eq!(
+        router.search(GET, "/api"),
+        Some(&mock_handler_1)
+    );
+    assert_eq!(
+        router.search(POST, "/api"),
+        Some(&mock_handler_2)
+    );
+}
+#[test]
+fn search_two_pararel_2() {
+    let (mock_handler_1, mock_handler_2) = (100, 200);
+
+    let mut router = Router::new();
+    router.register(GET, "/api", mock_handler_1);
+    router.register(GET, "/api_v2", mock_handler_2);
+
+    assert_eq!(
+        router.search(GET, "/api"),
+        Some(&mock_handler_1)
+    );
+    assert_eq!(
+        router.search(GET, "/api_v2"),
+        Some(&mock_handler_2)
+    );
+}
+
+#[test]
+fn search_two_nested() {
+    let (mock_handler_1, mock_handler_2) = (100, 200);
+
+    let mut router = Router::new();
+    router.register(GET, "/api", mock_handler_1);
+    router.register(GET, "/api/users", mock_handler_2);
+
+    assert_eq!(
+        router.search(GET, "/api"),
+        Some(&mock_handler_1)
+    );
+    assert_eq!(
+        router.search(GET, "/api/users"),
+        Some(&mock_handler_2)
+    );
+}
+
+#[test]
+fn search_with_param() {
+    let (mock_handler_1, mock_handler_2) = (100, 200);
+
+    let mut router = Router::new();
+    router.register(GET, "/api", mock_handler_1);
+    router.register(GET, "/api/:id", mock_handler_2);
+
+    assert_eq!(
+        router.search(GET, "/api"),
+        Some(&mock_handler_1)
+    );
+    assert_eq!(
+        router.search(GET, "/api/2"),
+        Some(&mock_handler_2)
+    );
+}
