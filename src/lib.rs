450f3be0ac8cb052999c476a3980fc3094a40b49
kana-rus <kanarus786@gmail.com>
2022-12-21 01:13:46 +0900

    Update: version

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/ohkami/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/ohkami/0.3.1")]
 
 #[cfg(all(not(feature = "sqlx"), any(feature = "postgres", feature = "mysql")))]
 compile_error!("feature `postgres` or `mysql` can't be enebled without enabling `sqlx` feature");
