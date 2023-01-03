7c3d40498520ee238c0b708f09383ae989f80753
kana-rus <kanarus786@gmail.com>
2023-01-03 18:37:12 +0900

    Fix: example

--- ohkami/examples/simple/src/bin/request_body.rs
+++ ohkami/examples/simple/src/bin/request_body.rs
@@ -30,7 +30,7 @@ mod test {
 
     #[test]
     fn test_api_login_j() {
-        let server = Server::default()
+        let server = Ohkami::default()
             .POST("/api/login_j", only_whose_name_starts_with_j_can_login);
 
         server.assert_to_res(
