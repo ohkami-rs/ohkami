0d6fbd9f7eca8d67ff38f200feaba9287e2386e6
kana-rus <kanarus786@gmail.com>
2023-01-05 00:31:26 +0900

    @2023-01-05 00:31+9:00

---
+++ ohkami_json/src/lib.rs
@@ -0,0 +1,61 @@
+#![feature(iter_next_chunk)]
+mod serialize;
+mod deserialize;
+
+pub trait JSON: Sized {
+    fn serialize(&self) -> String;
+    fn deserialize(string: &str) -> Result<Self, &str>;
+}
+
+#[cfg(test)]
+mod simple_types {
+    use crate::JSON;
+
+    #[derive(PartialEq, Debug)]
+    struct MyInt(
+        usize
+    );
+    impl JSON for MyInt {
+        fn serialize(&self) -> String {
+            self.0.to_string()
+        }
+        fn deserialize(string: &str) -> Result<Self, &str> {
+            match string.parse::<usize>() {
+                Ok(int) => Ok(Self(int)),
+                Err(_) => Err(string)
+            }
+        }
+    }
+    #[test]
+    fn json_int() {
+        let case = MyInt(123);
+        assert_eq!(case.serialize(), "123");
+        assert_eq!(<MyInt as JSON>::deserialize("123").unwrap(), case);
+    }
+
+    #[derive(PartialEq, Debug)]
+    struct MyString(
+        String
+    );
+    impl JSON for MyString {
+        fn serialize(&self) -> String {
+            format!(r#""{}""#, self.0)
+        }
+        fn deserialize(string: &str) -> Result<Self, &str> {
+            if string.starts_with('"')
+            && string.ends_with('"') {
+                Ok(Self(string[1..string.len()-1].to_owned()))
+            } else {
+                Err(string)
+            }
+        }
+    }
+    #[test]
+    fn json_str() {
+        let case = MyString(String::from("string!!!"));
+        assert_eq!(case.serialize(), r#""string!!!""#);
+        assert_ne!(case.serialize(), r#"string!!!"#);
+        assert_eq!(<MyString as JSON>::deserialize(r#""string!!!""#).unwrap(), case);
+        assert!(<MyString as JSON>::deserialize(r#"string!!!"#).is_err());
+    }
+}
---
+++ ohkami_json/src/serialize.rs
@@ -0,0 +1,51 @@
+pub trait Serialize {
+    fn serialize(&self) -> String;
+}
+
+impl Serialize for &'static str {
+    fn serialize(&self) -> String {
+        format!(r#""{self}""#)
+    }
+}
+impl Serialize for String {
+    fn serialize(&self) -> String {
+        format!(r#""{self}""#)
+    }
+}
+
+impl Serialize for bool {
+    fn serialize(&self) -> String {
+        String::from(if *self {"true"} else {"false"})
+    }
+}
+
+macro_rules! impl_for_int {
+    ($( $int:ty )*) => {
+        $(
+            impl Serialize for $int {
+                fn serialize(&self) -> String {
+                    self.to_string()
+                }
+            }
+        )*
+    };
+} impl_for_int!(
+    u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128
+);
+
+impl<S: Serialize> Serialize for Vec<S> {
+    fn serialize(&self) -> String {
+        self.into_iter().fold(
+            String::from("["),
+            |it, next| it + &next.serialize()
+        ) + "]"
+    }
+}
+impl<S: Serialize, const N: usize> Serialize for [S; N] {
+    fn serialize(&self) -> String {
+        self.into_iter().fold(
+            String::from("["),
+            |it, next| it + &next.serialize()
+        ) + "]"
+    }
+}
