83f8cae262ba537e0af258c3ca5dd972adc54080
kana-rus <kanarus786@gmail.com>
2023-02-19 20:29:31 +0900

    save@2023-02-19 20:29+9:00

--- ohkami/src/ohkami/config.rs
+++ ohkami/src/ohkami/config.rs
@@ -2,6 +2,14 @@ use async_std::sync::{Arc, Mutex};
 use tracing_subscriber::fmt::SubscriberBuilder;
 use crate::error::Error;
 
+
+#[cfg(feature="sqlx-postgres")]
+use sqlx::PgPool as ConnectionPool;
+#[cfg(feature="sqlx-mysql")]
+use sqlx::MySqlPool as ConnectionPool;
+#[cfg(feature="deadpool-postgres")]
+const _: (/* WIP */) = {};
+
 pub static CONFIG: Config = Config(
     Arc::new(Mutex::new(OhkamiConfig {
         log_subscribe:
@@ -9,6 +17,12 @@ pub static CONFIG: Config = Config(
                 tracing_subscriber::fmt()
                     .with_max_level(tracing::Level::DEBUG)
             ),
+        #[cfg(any(
+            feature="sqlx-postgres",
+            feature="sqlx-mysql",
+            feature="deadpool-postgres",
+        ))]
+        connection_pool: None,
     }))
 );
 
@@ -16,7 +30,7 @@ pub(crate) struct OhkamiConfig {
     pub(crate) log_subscribe: Option<SubscriberBuilder>,
 
     #[cfg(any(feature="sqlx-postgres", feature="sqlx-mysql", feature="deadpool-postgres"))]
-    connection_pool: ConnectionPool,
+    connection_pool: Option<ConnectionPool>,
 }
 // impl Clone for OhkamiConfig {
 //     fn clone(&self) -> Self {
@@ -36,6 +50,11 @@ pub struct Config(
         pub fn log_subscribe<LSC: LogSubscribeConfig>(mut self, log_subscribe_config: LSC) -> Self {
             self.0.get_mut().log_subscribe = log_subscribe_config.value();
             self
+        }
+
+        #[cfg(any(feature="sqlx-postgres", feature="sqlx-mysql"))]
+        pub fn connection_pool(mut self, connection_pool: ConnectionPool) -> Self {
+            todo!()
         }
     }
 
