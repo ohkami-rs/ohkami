88a94447329e1bac72524653b5d23a28189d17aa
kana-rus <kanarus786@gmail.com>
2023-06-30 19:52:30 +0900

    Rm: unused comment

--- ohkami/src/layer5_ohkami/build.rs
+++ ohkami/src/layer5_ohkami/build.rs
@@ -5,58 +5,6 @@ use crate::{
     layer4_router::{TrieRouter},
 };
 
-/*
-/// <br/>
-/// 
-/// ```ignore
-/// async fn main() -> Result<()> {
-///     let api_ohkami = Ohkami(
-///         "/users"
-///             .POST(create_user),
-///         "/users/:id"
-///             .GET(get_user_by_id)
-///             .PATCH(update_user),
-///     );
-/// 
-///     // I'd like to use `auth` and `log` fang...
-///     
-///     let api_ohkami = Ohkami.with((auth, log))(
-///         "/users"
-///             .POST(create_user),
-///         "/users/:id"
-///             .GET(get_user_by_id)
-///             .PATCH(update_user),
-///     );
-/// 
-///     // (Actually, this `log` fang of api_ohkami is duplicated with
-///     // `log` fang of the root ohkami below, but there's no problem
-///     // because they are merged internally.)
-/// 
-///     Ohkami.with((log))(
-///         "/hc" .GET(health_check),
-///         "/api".by(api_ohkami),
-///     ).howl(3000).await
-/// }
-/// ```
-#[allow(non_upper_case_globals)]
-pub const Ohkami: init::Ohkami = init::Ohkami;
-
-mod init {
-    use crate::layer5_ohkami::with_fangs::Fangs;
-
-    pub struct Ohkami;
-    impl Ohkami {
-        pub fn with<G>(self, fangs: impl Fangs<G>) -> super::with_fangs::Ohkami {
-            super::with_fangs::Ohkami(fangs.collect())
-        }
-    }
-}
-mod with_fangs {
-    use crate::layer3_fang_handler::Fang;
-
-    pub struct Ohkami(pub(super) Vec<Fang>);
-}
-*/
 
 trait Rounting {
     fn apply(self, routes: TrieRouter) -> TrieRouter;
