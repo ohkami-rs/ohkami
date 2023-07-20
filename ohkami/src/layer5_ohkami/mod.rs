808481d2a8f6a08220e19351dde9275d25a31b0e
kana-rus <kanarus786@gmail.com>
2023-07-21 05:05:38 +0900

    Update: doc comment

--- ohkami/src/layer5_ohkami/mod.rs
+++ ohkami/src/layer5_ohkami/mod.rs
@@ -11,35 +11,46 @@ use crate::{
 /// <br/>
 /// 
 /// ```ignore
-/// async fn main() -> Result<()> {
+/// async fn main() {
 ///     let api_ohkami = Ohkami::new()(
-///         "/users"
-///             .POST(create_user),
-///         "/users/:id"
-///             .GET(get_user_by_id)
-///             .PATCH(update_user),
+///         "/users".
+///             POST(create_user),
+///         "/users/:id".
+///             GET(get_user_by_id).
+///             PATCH(update_user),
 ///     );
 /// 
 ///     // I'd like to use `auth` and `log` fang...
 ///     
 ///     let api_ohkami = Ohkami::with((auth, log))(
-///         "/users"
-///             .POST(create_user),
-///         "/users/:id"
-///             .GET(get_user_by_id)
-///             .PATCH(update_user),
+///         "/users".
+///             POST(create_user),
+///         "/users/:id".
+///             GET(get_user_by_id).
+///             PATCH(update_user),
 ///     );
 /// 
 ///     // (Actually, this `log` fang of api_ohkami is duplicated with
 ///     // `log` fang of the root ohkami below, but there's no problem
 ///     // because they are merged internally.)
 /// 
-///     Ohkami::with((log))(
+///     Ohkami::with((log,))(
 ///         "/hc" .GET(health_check),
 ///         "/api".by(api_ohkami),
 ///     ).howl(3000).await
 /// }
 /// ```
+/// 
+/// <br/>
+/// 
+/// ## fang schema
+/// - front
+///   - `(&{mut}Context) -> ()`
+///   - `(&{mut}Context, &Request) -> ()`
+///   - and returning `Result<(), Response>` version of them
+/// - back
+///   - `(Response) -> Response`
+///   - `(Response) -> Result<Response, Response>`
 pub struct Ohkami {
     pub(crate) routes: TrieRouter,
 
