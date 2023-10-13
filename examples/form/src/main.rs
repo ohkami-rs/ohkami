b98f321de742b3a21ed0db56b793887f90841f26
kana-rus <kanarus786@gmail.com>
2023-10-13 19:28:24 +0900

    @2023-10-13 19:28+9:00

--- examples/form/src/main.rs
+++ examples/form/src/main.rs
@@ -1,17 +1,31 @@
+use ohkami::prelude::*;
 use ohkami::utils::{Payload, File};
 
 #[Payload(FormData)]
-struct SubmitData1 {
-    account_name: String,
-}
-
-#[Payload(FormData)]
-struct SubmitData2 {
+struct FormData {
     account_name: String,
     pics:         Vec<File>,
 }
 
+#[Payload(FormData)]
+struct FormData2 { /* Should be NOT ACCEPTABLE */
+    account_name: Option<String>,
+    pics:         Vec<File>,
+}
+
+async fn get_form(c: Context) -> Response {
+    c.OK().html(include_str!("../form.html"))
+}
+
+async fn post_submit(c: Context, form_data: FormData) -> Response {
+
+    c.NoContent()
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    Ohkami::new((
+        "/form"  .GET(get_form),
+        "/submit".POST(post_submit),
+    )).howl(5000).await
 }
