843f26125384e84e78f0608f5ea3dbf5cea70e81
kana-rus <kanarus786@gmail.com>
2023-12-30 20:38:19 +0900

    `utils::now`: eleiminate components

--- ohkami/src/x_utils/now.rs
+++ ohkami/src/x_utils/now.rs
@@ -69,7 +69,7 @@ impl UTCDateTime {
             }
 
             buf.push(' ');
-            buf.push_str(SHORT_MONTHS[date.month0() as usize]);
+            buf.push_str(SHORT_MONTHS[date.month_index() as usize]);
 
             buf.push(' ');
             let year = date.year();
@@ -91,6 +91,84 @@ impl UTCDateTime {
     }
 }
 
+/// (year << 13) | of
+#[derive(Debug, PartialEq)]
+struct Date(i32);
+impl Date {
+    fn from_days(days: i32) -> Self {
+        const YEAR_DELTAS: &[u8; 401] = &[
+            0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8,
+            8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14,
+            15, 15, 15, 15, 16, 16, 16, 16, 17, 17, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20, 20, 20,
+            21, 21, 21, 21, 22, 22, 22, 22, 23, 23, 23, 23, 24, 24, 24, 24, 25, 25, 25, // 100
+            25, 25, 25, 25, 25, 26, 26, 26, 26, 27, 27, 27, 27, 28, 28, 28, 28, 29, 29, 29, 29, 30, 30, 30,
+            30, 31, 31, 31, 31, 32, 32, 32, 32, 33, 33, 33, 33, 34, 34, 34, 34, 35, 35, 35, 35, 36, 36, 36,
+            36, 37, 37, 37, 37, 38, 38, 38, 38, 39, 39, 39, 39, 40, 40, 40, 40, 41, 41, 41, 41, 42, 42, 42,
+            42, 43, 43, 43, 43, 44, 44, 44, 44, 45, 45, 45, 45, 46, 46, 46, 46, 47, 47, 47, 47, 48, 48, 48,
+            48, 49, 49, 49, // 200
+            49, 49, 49, 49, 49, 50, 50, 50, 50, 51, 51, 51, 51, 52, 52, 52, 52, 53, 53, 53, 53, 54, 54, 54,
+            54, 55, 55, 55, 55, 56, 56, 56, 56, 57, 57, 57, 57, 58, 58, 58, 58, 59, 59, 59, 59, 60, 60, 60,
+            60, 61, 61, 61, 61, 62, 62, 62, 62, 63, 63, 63, 63, 64, 64, 64, 64, 65, 65, 65, 65, 66, 66, 66,
+            66, 67, 67, 67, 67, 68, 68, 68, 68, 69, 69, 69, 69, 70, 70, 70, 70, 71, 71, 71, 71, 72, 72, 72,
+            72, 73, 73, 73, // 300
+            73, 73, 73, 73, 73, 74, 74, 74, 74, 75, 75, 75, 75, 76, 76, 76, 76, 77, 77, 77, 77, 78, 78, 78,
+            78, 79, 79, 79, 79, 80, 80, 80, 80, 81, 81, 81, 81, 82, 82, 82, 82, 83, 83, 83, 83, 84, 84, 84,
+            84, 85, 85, 85, 85, 86, 86, 86, 86, 87, 87, 87, 87, 88, 88, 88, 88, 89, 89, 89, 89, 90, 90, 90,
+            90, 91, 91, 91, 91, 92, 92, 92, 92, 93, 93, 93, 93, 94, 94, 94, 94, 95, 95, 95, 95, 96, 96, 96,
+            96, 97, 97, 97, 97, // 400+1
+        ];
+
+        let days = days + 365;
+        let year_div_400 = days.div_euclid(146_097);
+        let cycle = days.rem_euclid(146_097) as u32;
+
+        let mut year_mod_400 = cycle / 365;
+        let mut ordinal = cycle % 365 + 1;
+        let delta = unsafe {*YEAR_DELTAS.get_unchecked(year_mod_400 as usize)} as u32;
+        if ordinal <= delta {
+            year_mod_400 -= 1;
+            ordinal += 365 - unsafe {*YEAR_DELTAS.get_unchecked(year_mod_400 as usize)} as u32;
+        } else {
+            ordinal -= delta;
+        }
+
+        let flags = YearFlag::from_year(year_mod_400 as i32);
+        Self::from_ordinal_and_flags(year_div_400 * 400 + year_mod_400 as i32, ordinal, flags)
+    }
+    fn from_ordinal_and_flags(
+        year:    i32,
+        ordinal: u32,
+        flag:    YearFlag,
+    ) -> Date {
+        const MAX_YEAR: i32 = i32::MAX >> 13;
+        const MIN_YEAR: i32 = i32::MIN >> 13;
+        
+        debug_assert!(year >= MIN_YEAR && year <= MAX_YEAR, "Year out of range");
+        debug_assert!(YearFlag::from_year(year).0 == flag.0);
+
+        let of = Of::new(ordinal, flag);
+        Self((year << 13) | (of.0 as i32))
+    }
+
+    const fn year(&self) -> i32 {
+        self.0 >> 13
+    }
+    const fn month_index(&self) -> u32 {
+        self.of().to_mdf().month() - 1
+    }
+    const fn day(&self) -> u32 {
+        self.of().to_mdf().day()
+    }
+    const fn weekday(&self) -> Weekday {
+        self.of().weekday()
+    }
+    #[inline]
+    const fn of(&self) -> Of {
+        Of::from_date(self.0)
+    }
+}
+
+#[derive(Debug, PartialEq)]
 struct Time {
     secs: u32,
     frac: u32,
@@ -115,73 +193,25 @@ struct Time {
     }
 }
 
-/// (year << 13) | of
-struct Date(i32);
-impl Date {
-    const fn from_days(days: i32) -> Self {
-        const fn cycle_to_yo(cycle: u32) -> (u32, u32) {
-            let mut year_mod_400 = cycle / 365;
-            let mut ordinal0 = cycle % 365;
-            let delta = YEAR_DELTAS[year_mod_400 as usize] as u32;
-            if ordinal0 < delta {
-                year_mod_400 -= 1;
-                ordinal0 += 365 - YEAR_DELTAS[year_mod_400 as usize] as u32;
-            } else {
-                ordinal0 -= delta;
-            }
-            (year_mod_400, ordinal0 + 1)
-        }
-
-        let days = days + 365;
-        let year_div_400 = days.div_euclid(146_097);
-        let cycle = days.rem_euclid(146_097);
-        let (year_mod_400, ordinal) = cycle_to_yo(cycle as u32);
-        let flags = YearFlag::from_year(year_mod_400 as i32);
-        Self::from_ordinal_and_flags(year_div_400 * 400 + year_mod_400 as i32, ordinal, flags)
-    }
-    const fn from_ordinal_and_flags(
-        year:    i32,
-        ordinal: u32,
-        flag:    YearFlag,
-    ) -> Date {
-        const MAX_YEAR: i32 = i32::MAX >> 13;
-        const MIN_YEAR: i32 = i32::MIN >> 13;
-        
-        debug_assert!(year >= MIN_YEAR && year <= MAX_YEAR, "Year out of range");
-        debug_assert!(YearFlag::from_year(year).0 == flag.0);
-
-        let of = Of::new(ordinal, flag);
-        Self((year << 13) | (of.0 as i32))
-    }
-
-    const fn year(&self) -> i32 {
-        self.0 >> 13
-    }
-    const fn month(&self) -> u32 {
-        self.mdf().month()
-    }
-    const fn month0(&self) -> u32 {
-        self.month() - 1
-    }
-    const fn day(&self) -> u32 {
-        self.mdf().day()
-    }
-    const fn weekday(&self) -> Weekday {
-        self.of().weekday()
-    }
-    const fn mdf(&self) -> Mdf {
-        self.of().to_mdf()
-    }
-    #[inline]
-    const fn of(&self) -> Of {
-        Of::from_date(self.0)
-    }
-}
-
 #[derive(Clone, Copy)]
 struct YearFlag(u8);
 impl YearFlag {
-    const fn from_year(year: i32) -> Self {
+    fn from_year(year: i32) -> Self {
+        const A:  YearFlag = YearFlag(0o15);
+        const AG: YearFlag = YearFlag(0o05);
+        const B:  YearFlag = YearFlag(0o14);
+        const BA: YearFlag = YearFlag(0o04);
+        const C:  YearFlag = YearFlag(0o13);
+        const CB: YearFlag = YearFlag(0o03);
+        const D:  YearFlag = YearFlag(0o12);
+        const DC: YearFlag = YearFlag(0o02);
+        const E:  YearFlag = YearFlag(0o11);
+        const ED: YearFlag = YearFlag(0o01);
+        const F:  YearFlag = YearFlag(0o17);
+        const FE: YearFlag = YearFlag(0o07);
+        const G:  YearFlag = YearFlag(0o16);
+        const GF: YearFlag = YearFlag(0o06);
+
         const YEAR_TO_FLAG: &[YearFlag; 400] = &[
             BA, G, F, E, DC, B, A, G, FE, D, C, B, AG, F, E, D, CB, A, G, F, ED, C, B, A, GF, E, D, C, BA,
             G, F, E, DC, B, A, G, FE, D, C, B, AG, F, E, D, CB, A, G, F, ED, C, B, A, GF, E, D, C, BA, G,
@@ -200,22 +230,8 @@ impl YearFlag {
             E, D, CB, A, G, F, ED, C, B, A, GF, E, D, C, BA, G, F, E, DC, B, A, G, FE, D, C, B, AG, F, E,
             D, CB, A, G, F, ED, C, B, A, GF, E, D, C, // 400
         ];
-        const A:  YearFlag = YearFlag(0o15);
-        const AG: YearFlag = YearFlag(0o05);
-        const B:  YearFlag = YearFlag(0o14);
-        const BA: YearFlag = YearFlag(0o04);
-        const C:  YearFlag = YearFlag(0o13);
-        const CB: YearFlag = YearFlag(0o03);
-        const D:  YearFlag = YearFlag(0o12);
-        const DC: YearFlag = YearFlag(0o02);
-        const E:  YearFlag = YearFlag(0o11);
-        const ED: YearFlag = YearFlag(0o01);
-        const F:  YearFlag = YearFlag(0o17);
-        const FE: YearFlag = YearFlag(0o07);
-        const G:  YearFlag = YearFlag(0o16);
-        const GF: YearFlag = YearFlag(0o06);
 
-        YEAR_TO_FLAG[year.rem_euclid(400) as usize]
+        unsafe {*YEAR_TO_FLAG.get_unchecked(year.rem_euclid(400) as usize)}
     }
 }
 
@@ -337,25 +353,3 @@ impl Weekday {
         (*self as u32 + 7 - Self::Sun as u32) % 7
     }
 }
-
-const YEAR_DELTAS: &[u8; 401] = &[
-    0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8,
-    8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14,
-    15, 15, 15, 15, 16, 16, 16, 16, 17, 17, 17, 17, 18, 18, 18, 18, 19, 19, 19, 19, 20, 20, 20, 20,
-    21, 21, 21, 21, 22, 22, 22, 22, 23, 23, 23, 23, 24, 24, 24, 24, 25, 25, 25, // 100
-    25, 25, 25, 25, 25, 26, 26, 26, 26, 27, 27, 27, 27, 28, 28, 28, 28, 29, 29, 29, 29, 30, 30, 30,
-    30, 31, 31, 31, 31, 32, 32, 32, 32, 33, 33, 33, 33, 34, 34, 34, 34, 35, 35, 35, 35, 36, 36, 36,
-    36, 37, 37, 37, 37, 38, 38, 38, 38, 39, 39, 39, 39, 40, 40, 40, 40, 41, 41, 41, 41, 42, 42, 42,
-    42, 43, 43, 43, 43, 44, 44, 44, 44, 45, 45, 45, 45, 46, 46, 46, 46, 47, 47, 47, 47, 48, 48, 48,
-    48, 49, 49, 49, // 200
-    49, 49, 49, 49, 49, 50, 50, 50, 50, 51, 51, 51, 51, 52, 52, 52, 52, 53, 53, 53, 53, 54, 54, 54,
-    54, 55, 55, 55, 55, 56, 56, 56, 56, 57, 57, 57, 57, 58, 58, 58, 58, 59, 59, 59, 59, 60, 60, 60,
-    60, 61, 61, 61, 61, 62, 62, 62, 62, 63, 63, 63, 63, 64, 64, 64, 64, 65, 65, 65, 65, 66, 66, 66,
-    66, 67, 67, 67, 67, 68, 68, 68, 68, 69, 69, 69, 69, 70, 70, 70, 70, 71, 71, 71, 71, 72, 72, 72,
-    72, 73, 73, 73, // 300
-    73, 73, 73, 73, 73, 74, 74, 74, 74, 75, 75, 75, 75, 76, 76, 76, 76, 77, 77, 77, 77, 78, 78, 78,
-    78, 79, 79, 79, 79, 80, 80, 80, 80, 81, 81, 81, 81, 82, 82, 82, 82, 83, 83, 83, 83, 84, 84, 84,
-    84, 85, 85, 85, 85, 86, 86, 86, 86, 87, 87, 87, 87, 88, 88, 88, 88, 89, 89, 89, 89, 90, 90, 90,
-    90, 91, 91, 91, 91, 92, 92, 92, 92, 93, 93, 93, 93, 94, 94, 94, 94, 95, 95, 95, 95, 96, 96, 96,
-    96, 97, 97, 97, 97, // 400+1
-];
