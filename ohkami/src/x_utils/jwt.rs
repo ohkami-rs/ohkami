b0f5308883ea0485e4577cafeff7341ea79b98eb
kana-rus <kanarus786@gmail.com>
2024-01-02 01:56:05 +0900

    @2023-01-02 01:55+9:00

--- ohkami/src/x_utils/jwt.rs
+++ ohkami/src/x_utils/jwt.rs
@@ -40,6 +40,15 @@ mod internal {
                     VerifyingAlgorithm::$default
                 }
             }
+            impl VerifyingAlgorithm {
+                const fn as_str(&self) -> &'static str {
+                    match self {
+                        $(
+                            Self::$alg => stringify!($alg),
+                        )*
+                    }
+                }
+            }
 
             impl JWT {
                 $(
@@ -61,17 +70,39 @@ mod internal {
 
     impl IntoFang for JWT {
         fn into_fang(self) -> Fang {
-            struct JWTHeader {
-                cty: ,
+            type Header  = ::serde_json::Value;
+            type Payload = ::serde_json::Value;
+            fn now() -> u64 {
+                use std::time::{SystemTime, UNIX_EPOCH};
+                SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs()
             }
 
-            Fang(|c: &Context, req: &Request| {
+            Fang(move |c: &Context, req: &Request| {
                 let mut parts = req
                     .headers.Authorization().ok_or_else(|| c.Unauthorized())?
                     .strip_prefix("Bearer ").ok_or_else(|| c.BadRequest())?
                     .split('.');
 
-                let header = base64_decode(parts.next().ok_or_else(|| c.BadRequest())?);
+                let header_part = parts.next()
+                    .ok_or_else(|| c.BadRequest())?;
+                let header: Header = ::serde_json::from_slice(&base64_decode(header_part))
+                    .map_err(|_| c.InternalServerError())?;
+                if header.get("typ").is_some_and(|typ| typ.as_str().unwrap_or("").eq_ignore_ascii_case("JWT")) {
+                    return Err(c.BadRequest())
+                }
+                if header.get("cty").is_some_and(|cty| cty.as_str().unwrap_or("").eq_ignore_ascii_case("JWT")) {
+                    return Err(c.BadRequest())
+                }
+                if header.get("alg").ok_or_else(|| c.BadRequest())? != self.alg.as_str() {
+                    return Err(c.BadRequest())
+                }
+
+                let payload_part = parts.next()
+                    .ok_or_else(|| c.BadRequest())?;
+                let payload: Payload = ::serde_json::from_slice(&base64_decode(payload_part))
+                    .map_err(|_| c.InternalServerError())?;
+                let now = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+                if payload.get("nbf").is_some_and(|nbf| nbf.)
 
                 Ok(())
             })
