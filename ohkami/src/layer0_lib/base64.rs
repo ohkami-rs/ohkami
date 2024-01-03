7cf507c17001132a00c76348b65638c0d10c28d9
kana-rus <kanarus786@gmail.com>
2024-01-03 23:38:07 +0900

    TODO: decode_quantum

--- ohkami/src/layer0_lib/base64.rs
+++ ohkami/src/layer0_lib/base64.rs
@@ -78,14 +78,21 @@ fn encode_by(src: &[u8], encode_map: &[u8; 64], padding: Option<u8>) -> String {
 }
 
 fn decode_by(encoded: &[u8], encode_map: &[u8; 64], padding: Option<u8>) -> Vec<u8> {
-    fn assembl64(n: [u8; 8]) -> Option<u64> {
+    fn assemble64(n: [u8; 8]) -> Option<u64> {
         let [n1, n2, n3, n4, n5, n6, n7, n8] = n.map(<u8 as Into<u64>>::into);
-        
         (n1|n2|n3|n4|n5|n6|n7|n8 != 0xff).then_some(
             n1<<58 | n2<<52 | n3<<46 | n4<<40 | n5<<34 | n6<<28 | n7<<22 | n8<<16
         )
     }
-
+    fn assemble32(n: [u8; 4]) -> Option<u32> {
+        let [n1, n2, n3, n4] = n.map(<u8 as Into<u32>>::into);
+        (n1|n2|n3|n4 != 0xff).then_some(
+            n1<<26 | n2<<20 | n3<<14 | n4<<8
+        )
+    }
+    fn decode_quantum(dst: &mut Vec<u8>, encoded: &[u8], si: usize) -> (usize, usize) {
+        todo!()
+    }
 
     let mut decoded = vec![u8::default(); encoded.len()];
     if decoded.is_empty() {return decoded}
@@ -98,12 +105,40 @@ fn decode_by(encoded: &[u8], encode_map: &[u8; 64], padding: Option<u8>) -> Vec<
         map
     };
 
+    let mut n  = 0;
     let mut si = 0;
 
     #[cfg(target_pointer_width = "64")]
-    while encoded.len() - si >= 8 {
-        let encoded2 = &encoded[si..(si + 8)];
+    while encoded.len() - si >= 8 && decoded.len() - n >= 8 {
+        let encoded2: [_; 8] = encoded[si..(si + 8)].try_into().unwrap();
+        if let Some(dn) = assemble64(encoded2.map(|byte| decode_map[byte as usize])) {
+            decoded[n..(n + 8)].copy_from_slice(&dn.to_be_bytes());
+            n  += 6;
+            si += 8;
+        } else {
+            let (new_si, n_inc) = decode_quantum(&mut decoded, encoded, si);
+            n += n_inc;
+            si = new_si;
+        }
+    }
 
+    while encoded.len() - si >= 4 && decoded.len() - n >= 4 {
+        let encoded2: [_; 4] = encoded[si..(si + 4)].try_into().unwrap();
+        if let Some(dn) = assemble32(encoded2.map(|byte| decode_map[byte as usize])) {
+            decoded[n..(n + 4)].copy_from_slice(&dn.to_be_bytes());
+            n  += 3;
+            si += 4;
+        } else {
+            let (new_si, n_inc) = decode_quantum(&mut decoded, encoded, si);
+            n += n_inc;
+            si = new_si;
+        }
+    }
+
+    while si < encoded.len() {
+        let (new_si, n_inc) = decode_quantum(&mut decoded, encoded, si);
+        n += n_inc;
+        si = new_si;
     }
 
     decoded.shrink_to_fit();
