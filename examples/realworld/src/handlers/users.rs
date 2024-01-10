ce223167eebde49b0d7a044dcc5a464e6aa63ff9
kana-rus <kanarus786@gmail.com>
2024-01-11 02:46:39 +0900

    Fix: `IntoHandler`: remove `Sync` bound of `Fut`

--- examples/realworld/src/handlers/users.rs
+++ examples/realworld/src/handlers/users.rs
@@ -43,46 +43,15 @@ struct RegisterRequest<'req> {
 
 async fn register(
     RegisterRequest { username, email, password }: RegisterRequest<'_>,
-    //pool: ohkami::Memory<'_, sqlx::PgPool>,
 ) -> Result<JSON<UserResponse>, RealWorldError> {
-    let _ = sqlx::query("").execute(pool()).await;
-    // sqlx::query!(r#"
-    //     SELECT id
-    //     FROM users AS u
-    //     WHERE
-    //         u.name = $1  AND
-    //         u.email = $2
-    //         -- TODO
-    // "#, username, email).fetch_optional(pool()).await.map_err(RealWorldError::DB)?;
+    sqlx::query!(r#"
+        SELECT id
+        FROM users AS u
+        WHERE
+            u.name = $1  AND
+            u.email = $2
+            -- TODO
+    "#, username, email).fetch_optional(pool()).await.map_err(RealWorldError::DB)?;
 
-    Ok(JSON::Created(UserResponse {
-        user: User {
-            email:    String::new(),
-            token:    String::new(),
-            username: String::new(),
-            bio:      None,
-            image:    None,
-        }
-    }))
-}
-
-#[cfg(test)] fn __() {
-    fn assert_fn1<
-        'req,
-        F:   Fn(B) -> Fut + Send + Sync + 'static,
-        B:   ohkami::FromRequest<'req>,
-        Fut: std::future::Future<Output = Res> + 'static,
-        Res: ohkami::IntoResponse,
-    >(_: F) {}
-    fn assert_fn2<
-        'req,
-        F:   Fn(B1, B2) -> Fut + Send + Sync + 'static,
-        B1:  ohkami::FromRequest<'req>,
-        B2:  ohkami::FromRequest<'req>,
-        Fut: std::future::Future<Output = Res> + 'static,
-        Res: ohkami::IntoResponse,
-    >(_: F) {}
-
-    assert_fn1(login);
-    assert_fn1(register);
+    todo!()
 }
