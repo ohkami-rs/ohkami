8ae88c9f236eddbea24ca957b74c736327b98713
kana-rus <kanarus786@gmail.com>
2024-04-11 00:51:58 +0900

    Add: doc comment for `Payload`, `PayloadType`

--- ohkami/src/typed/payload.rs
+++ ohkami/src/typed/payload.rs
@@ -2,15 +2,113 @@ use crate::{FromRequest, IntoResponse, Response};
 use serde::{Serialize, Deserialize};
 
 
+/// # Request/Response Payload
+/// 
+/// <br>
+/// 
+/// - `T` can be used as request body if `T: Payload + Deserialize`
+/// - `T` can be used as response body if `T: Payload + Serialize`
+/// 
+/// in ohkami's *typed* system.
+/// 
+/// It's recommended to impl this by `#[Payload]` attribute with a `PayloadType` argument.
+/// 
+/// <br>
+/// 
+/// ---
+/// *base.rs*
+/// ```
+/// /* This trait and the attribute */
+/// use ohkami::typed::Payload;
+/// 
+/// /* A `PayloadType` for application/json payload */
+/// use ohkami::builtin::payload::JSON;
+/// 
+/// use ohkami::serde::{Deserialize, Serialize};
+/// 
+/// #[Payload]
+/// #[derive(
+///     Deserialize, // derive to use `User` as request body
+///     Serialize    // derive to use `User` as response body
+/// )]
+/// struct User {
+///     id:   usize,
+///     name: String,
+/// }
+/// ```
+/// ---
+/// 
+/// <br>
+/// 
+/// We derive `Deserialize` or `Serialize` in most cases, so `#[Payload]` supports shorthands for that:
+/// 
+/// <br>
+/// 
+/// ---
+/// *shorthand.rs*
+/// ```
+/// use ohkami::typed::Payload;
+/// use ohkami::builtin::payload::JSON;
+/// 
+/// #[Payload(JSON/DS)]
+/// struct User {
+///     id:   usize,
+///     name: String,
+/// }
+/// ```
+/// ---
+/// 
+/// <br>
+/// 
+/// After `/`,
+/// 
+/// - `D` automatically derives `Desrerialize` for the struct
+/// - `S` automatically derives `Serialize` for the struct
+/// 
+/// respectively.
 pub trait Payload: Sized {
     type Type: PayloadType;
 }
 
 pub trait PayloadType {
+    /// Mime type like `application/json`, `text/plain`, ...
+    /// 
+    /// Used for checking `Content-Type` of a request.
     const MIME_TYPE: &'static str;
+    
+    /// Just mime type, or maybe it with some additional information, like `application/json`, `text/plain; charset=UTF-8`, ...
+    /// 
+    /// Used for `Content-Type` of a response with the `Payload` type.
     const CONTENT_TYPE: &'static str = Self::MIME_TYPE;
 
+    /// Deserializing logic for parsing a request body that should be the `Payload` type.
+    /// 
+    /// <br>
+    /// 
+    /// ---
+    /// *example.rs*
+    /// ```
+    /// # use serde::Deserialize;
+    /// fn parse<'req, T: Deserialize<'req>>(bytes: &'req [u8]) -> Result<T, impl crate::serde::de::Error> {
+    ///     ::serde_json::from_slice(bytes)
+    /// }
+    /// ```
+    /// ---
     fn parse<'req, T: Deserialize<'req>>(bytes: &'req [u8]) -> Result<T, impl crate::serde::de::Error>;
+
+    /// Serializing logic for a response body of the `Payload` type.
+    /// 
+    /// <br>
+    /// 
+    /// ---
+    /// *example.rs*
+    /// ```
+    /// # use serde::Serialize;
+    /// fn bytes<T: Serialize>(value: &T) -> Result<Vec<u8>, impl crate::serde::ser::Error> {
+    ///     ::serde_json::to_vec(&value)
+    /// }
+    /// ```
+    /// ---
     fn bytes<T: Serialize>(value: &T) -> Result<Vec<u8>, impl crate::serde::ser::Error>;
 }
 
