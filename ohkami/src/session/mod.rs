18f662bc8fbbd6d4b9d2587f834e8fa814ba365b
kanarus <kanarus786@gmail.com>
2024-05-03 06:20:05 +0900

    Add Keep-Alive timeout (#144)
    * Add Keep-Alive timeout (42 secs, hardcoded now) and `connection.shutdown` at the end of `Session::manage`
    
    * not DEBUG
    
    * Fix: when `rt_async-std` shutdown in async-std's way

--- ohkami/src/session/mod.rs
+++ ohkami/src/session/mod.rs
@@ -1,9 +1,8 @@
 #![cfg(any(feature="rt_tokio",feature="rt_async-std"))]
 
-use std::any::Any;
-use std::{pin::Pin, sync::Arc};
+use std::{any::Any, pin::Pin, sync::Arc, future::Future, time::Duration, task::Poll};
 use std::panic::{AssertUnwindSafe, catch_unwind};
-use crate::__rt__::{TcpStream};
+use crate::__rt__::{TcpStream, sleep};
 use crate::ohkami::router::RadixRouter;
 use crate::{Request, Response};
 
@@ -35,23 +34,52 @@ impl Session {
             crate::Response::InternalServerError()
         }
 
-        let connection = &mut self.connection;
-        loop {
-            let mut req = Request::init();
-            let mut req = unsafe {Pin::new_unchecked(&mut req)};
-            match req.as_mut().read(connection).await {
-                Ok(Some(())) => {
-                    let close = matches!(req.headers.Connection(), Some("close" | "Close"));
-                    let res = match catch_unwind(AssertUnwindSafe(|| self.router.handle(req.get_mut()))) {
-                        Ok(future) => future.await,
-                        Err(panic) => panicking(panic),
-                    };
-                    res.send(connection).await;
-                    if close {break}
+        fn keep_alive(
+            secs: u64,
+            proc: impl Future<Output = ()>,
+        ) -> impl Future<Output = ()> {
+            struct Timeout<Sleep, Proc> { sleep: Sleep, proc: Proc }
+
+            impl<Sleep: Future<Output = ()>, Proc: Future<Output = ()>> Future for Timeout<Sleep, Proc> {
+                type Output = ();
+                fn poll(mut self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
+                    unsafe {
+                        match self.as_mut().map_unchecked_mut(|t| &mut t.proc).poll(cx) {
+                            Poll::Ready(()) => Poll::Ready(()),
+                            Poll::Pending   => self.map_unchecked_mut(|t| &mut t.sleep).poll(cx),
+                        }
+                    }
                 }
-                Ok(None) => break,
-                Err(res) => res.send(connection).await,
-            };
+            }
+
+            Timeout { sleep: sleep(Duration::from_secs(secs)), proc }
+        }
+
+        let connection = &mut self.connection;
+        keep_alive(42/* TODO: make this configurable by user */, async {
+            loop {
+                let mut req = Request::init();
+                let mut req = unsafe {Pin::new_unchecked(&mut req)};
+                match req.as_mut().read(connection).await {
+                    Ok(Some(())) => {
+                        let close = matches!(req.headers.Connection(), Some("close" | "Close"));
+                        let res = match catch_unwind(AssertUnwindSafe(|| self.router.handle(req.get_mut()))) {
+                            Ok(future) => future.await,
+                            Err(panic) => panicking(panic),
+                        };
+                        res.send(connection).await;
+                        if close {break}
+                    }
+                    Ok(None) => break,
+                    Err(res) => res.send(connection).await,
+                };
+            }
+        }).await;
+        #[cfg(feature="rt_tokio")] {use crate::__rt__::AsyncWriter;
+            connection.shutdown().await.expect("Failed to shutdown stream");
+        }
+        #[cfg(feature="rt_async-std")] {
+            connection.shutdown(std::net::Shutdown::Both).expect("Failed to shutdown stream");
         }
     }
 }
