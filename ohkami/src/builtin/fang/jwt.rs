af71e13f3de9ecf48b9e55d52025cce3cfbe75bf
kanarus <kanarus786@gmail.com>
2024-05-18 05:22:55 +0900

    fix JWT: to not return error response for OPTIONS amd only put warning (#170)

--- ohkami/src/builtin/fang/jwt.rs
+++ ohkami/src/builtin/fang/jwt.rs
@@ -276,11 +276,8 @@ impl<Payload: for<'de> Deserialize<'de>> JWT<Payload> {
     /// Then it's valid, this returns decoded paylaod of the JWT as `Payload`.
     pub fn verified(&self, req: &Request) -> Result<Payload, Response> {
         (! req.method.isOPTIONS()).then_some(()).ok_or_else(|| {
-            crate::warning!(
-                "`ohkami::builtin::JWT` doesn't perform verifying for OPTIONS requests, \
-                and returns Internal Server Error if `JWT::verified` or `JWT::verify` is called."
-            );
-            Response::InternalServerError()
+            crate::warning!("`ohkami::builtin::JWT` doesn't perform verifying for OPTIONS requests");
+            Response::OK()
         })?;
 
         const UNAUTHORIZED_MESSAGE: &str = "missing or malformed jwt";
