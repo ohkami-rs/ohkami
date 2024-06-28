2341d4fd2598b4d6acb08c444376bb8f3c76e492
kana-rus <kanarus786@gmail.com>
2024-06-28 16:33:24 +0900

    & bump major version

--- ohkami_macros/src/worker.rs
+++ ohkami_macros/src/worker.rs
@@ -215,7 +215,7 @@ pub fn bindings(env: TokenStream, bindings_struct: TokenStream) -> Result<TokenS
         let methods = bindings.iter()
             .filter_map(|(name, binding)| match binding {
                 Binding::Variable(var) => Some(quote! {
-                    #vis const fn #name() -> &'static str { #var }
+                    #vis const #name: &'static str = #var;
                 }),
                 _ => None
             });
