a4c74e2cf2b817b42d131d1238d691941dabc451
kana-rus <kanarus786@gmail.com>
2024-07-23 14:18:15 +0900

    fix Handler: `Fn` -> `FnOnce`

--- examples/websocket/src/main.rs
+++ examples/websocket/src/main.rs
@@ -14,6 +14,7 @@ impl FangAction for Logger {
     }
 }
 
+
 async fn echo_text(c: WebSocketContext<'_>) -> WebSocket {
     c.connect(|mut ws| async move {
         #[cfg(feature="DEBUG")] {
@@ -44,10 +45,35 @@ async fn echo_text(c: WebSocketContext<'_>) -> WebSocket {
     })
 }
 
+
+async fn echo_text_2(
+    name: String,
+    ctx:  WebSocketContext<'_>
+) -> EchoTextSession<'_> {
+    EchoTextSession { name, ctx }
+}
+
+struct EchoTextSession<'ws> {
+    name: String,
+    ctx:  WebSocketContext<'ws>,
+}
+impl IntoResponse for EchoTextSession<'_> {
+    fn into_response(self) -> Response {
+        self.ctx.connect(|mut ws| async move {
+                ws.send(Message::Text(format!("Hello, {}!", self.name))).await.expect("failed to send");
+        
+                while let Ok(Some(Message::Text(text))) = ws.recv().await {
+                    ws.send(Message::Text(text)).await.expect("failed to send text");
+                }
+        }).into_response()
+    }
+}
+
 #[tokio::main]
 async fn main() {
     Ohkami::with(Logger, (
         "/".Dir("./template").omit_extensions([".html"]),
         "/echo".GET(echo_text),
+        "/echo2/:name".GET(echo_text_2)
     )).howl("localhost:3030").await
 }
