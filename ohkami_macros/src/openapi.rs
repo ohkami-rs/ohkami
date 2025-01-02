b9c597a35bdb6cc2f3659590f4a0f119b8391574
kanarus <kanarus786@gmail.com>
2025-01-02 22:18:18 +0900

    impl Case

--- ohkami_macros/src/openapi.rs
+++ ohkami_macros/src/openapi.rs
@@ -13,9 +13,69 @@ pub(super) fn derive_schema(input: TokenStream) -> syn::Result<TokenStream> {
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    struct OpenAPIAttributes {
-        inline:    bool,
-        component: Option<String>,
+    struct ContainerAttributes {
+        openapi_inline:    bool,
+        openapi_component: Option<String>,
+        serde_rename:            Option<String>,
+        serde_rename_all:        Option<Case>,
+        serde_rename_all_fields: Option<Case>,
+    }
+
+    // based on https://github.com/serde-rs/serde/blob/930401b0dd58a809fce34da091b8aa3d6083cb33/serde_derive/src/internals/case.rs
+    enum Case { Lower, Upper, Pascal, Camel, Snake, ScreamingSnake, Kebab, ScreamingKebab }
+    impl Case {
+        fn from_str(s: &str) -> Option<Self> {
+            match s {
+                "lowercase"            => Self::Lower,
+                "UPPERCASE"            => Self::Upper,
+                "PascalCase"           => Self::Pascal,
+                "camelCase"            => Self::Camel,
+                "snake_case"           => Self::Snake,
+                "SCREAMING_SNAKE_CASE" => Self::ScreamingSnake,
+                "kebab-case"           => Self::Kebab,
+                "SCREAMING-KEBAB-CASE" => Self::ScreamingKebab,
+            }
+        }
+        fn apply_to_field(self, field: &str) -> String {
+            match self {
+                Self::Lower | Self::Snake => field.to_string(),
+                Self::Upper => field.to_ascii_uppercase(),
+                Self::Pascal => field
+                    .split('_')
+                    .map(|s| s[..1].to_ascii_uppercase() + &s[1..])
+                    .collect(),
+                Self::Camel => {
+                    let pascal = Self::Pascal.apply_to_field(field);
+                    pascal[..1].to_ascii_lowercase() + &pascal[1..]
+                }
+                Self::ScreamingSnake => Self::Upper.apply_to_field(field),
+                Self::Kebab => field.replace('_', "-"),
+                Self::ScreamingKebab => Self::ScreamingSnake
+                    .apply_to_field(field)
+                    .replace('_', "-"),
+            }
+        }
+        fn apply_to_variant(self, variant: &str) -> String {
+            match self {
+                Self::Pascal => variant.to_string(),
+                Self::Lower => variant.to_ascii_lowercase(),
+                Self::Upper => variant.to_ascii_uppercase(),
+                Self::Camel => variant[..1].to_ascii_lowercase() + &variant[1..],
+                Self::Snake => variant
+                    .split(char::is_uppercase)
+                    .map(str::to_ascii_lowercase())
+                    .collect::<Vec<_>>().join("_"),
+                Self::ScreamingSnake => Self::Snake
+                    .apply_to_variant(variant)
+                    .to_ascii_uppercase(),
+                Self::Kebab => Self::Snake
+                    .apply_to_variant(variant)
+                    .replace('_', "-"),
+                Self::ScreamingKebab => Self::ScreamingSnake
+                    .apply_to_variant(variant)
+                    .replace('_', "-"),
+            }
+        }
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
