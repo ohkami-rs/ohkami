6846acd12d2bf77320b48e0935d5dd473bae9581
agam <145994211+voldtman@users.noreply.github.com>
2025-03-20 23:29:07 +0700

    tls: basic tls implementation with tokio-rustls (tokio-only!) (#441)
    * enhance(Dir): support conditional fetch with `If-None-Match` `If-Modified-Since`
    
    * next: test conditional fetch impl
    
    * tls: implement basic tls implementation with tokio-rustls (tokio-only!)
    
    * fix(session): fix AsyncRead & AsyncWrite imports for session
    
    * fix(session): fix unsupported WS over TLS & cfg handling
    
    * fix(session): fix trait bounds when ws feature is disabled
    
    * fix(session): omit Send trait to unbreak builds on other runtimes (hopefully)
    
    * fix(etag): merge issues fix
    
    ---------
    
    Co-authored-by: kanarus <kanarus786@gmail.com>

--- ohkami/src/session/mod.rs
+++ ohkami/src/session/mod.rs
@@ -2,23 +2,42 @@
 
 use std::{any::Any, pin::Pin, sync::Arc, time::Duration};
 use std::panic::{AssertUnwindSafe, catch_unwind};
-use crate::__rt__::TcpStream;
+use crate::__rt__::{AsyncRead, AsyncWrite};
 use crate::response::Upgrade;
 use crate::util::timeout_in;
 use crate::router::r#final::Router;
 use crate::{Request, Response};
 
-pub(crate) struct Session {
-    router:     Arc<Router>,
-    connection: TcpStream,
-    ip:         std::net::IpAddr,
+#[cfg(feature="ws")]
+use crate::__rt__::TcpStream;
+
+pub(crate) struct Session<S> {
+    router: Arc<Router>,
+    connection: S,
+    ip: std::net::IpAddr,
 }
 
-impl Session {
+#[cfg(feature="ws")]
+pub(crate) trait WebSocketUpgradeable {
+    fn into_websocket_stream(self) -> Result<TcpStream, &'static str>;
+}
+
+#[cfg(feature="ws")]
+impl WebSocketUpgradeable for TcpStream {
+    fn into_websocket_stream(self) -> Result<TcpStream, &'static str> {
+        Ok(self)
+    }
+}
+
+#[cfg(feature="ws")]
+impl<S> Session<S>
+where
+    S: AsyncRead + AsyncWrite + Unpin + WebSocketUpgradeable,
+{
     pub(crate) fn new(
-        router:     Arc<Router>,
-        connection: TcpStream,
-        ip:         std::net::IpAddr
+        router: Arc<Router>,
+        connection: S,
+        ip: std::net::IpAddr
     ) -> Self {
         Self {
             router,
@@ -31,11 +50,11 @@ impl Session {
         #[cold] #[inline(never)]
         fn panicking(panic: Box<dyn Any + Send>) -> Response {
             if let Some(msg) = panic.downcast_ref::<String>() {
-                crate::WARNING!("panic: {msg}");
+                crate::WARNING!("[Panicked]: {msg}");
             } else if let Some(msg) = panic.downcast_ref::<&str>() {
-                crate::WARNING!("panic: {msg}");
+                crate::WARNING!("[Panicked]: {msg}");
             } else {
-                crate::WARNING!("panic");
+                crate::WARNING!("[Panicked]");
             }
             crate::Response::InternalServerError()
         }
@@ -66,7 +85,7 @@ impl Session {
                 }
             }
         }).await {
-            None => crate::WARNING!("\
+            None => crate::WARNING!("[WARNING] \
                 Session timeouted. In Ohkami, Keep-Alive timeout \
                 is set to 42 seconds by default and is configurable \
                 by `OHKAMI_KEEPALIVE_TIMEOUT` environment variable.\
@@ -74,25 +93,99 @@ impl Session {
 
             Some(Upgrade::None) => crate::DEBUG!("about to shutdown connection"),
 
-            #[cfg(feature="ws")]
             Some(Upgrade::WebSocket(ws)) => {
-                crate::DEBUG!("WebSocket session started");
+                match self.connection.into_websocket_stream() {
+                    Ok(tcp_stream) => {
+                        crate::DEBUG!("WebSocket session started");
 
-                let aborted = ws.manage_with_timeout(
-                    Duration::from_secs(crate::CONFIG.websocket_timeout()),
-                    self.connection
-                ).await;
-                if aborted {
-                    crate::WARNING!("\
-                        WebSocket session aborted by timeout. In Ohkami, \
-                        WebSocket timeout is set to 3600 seconds (1 hour) \
-                        by default and is configurable by `OHKAMI_WEBSOCKET_TIMEOUT` \
-                        environment variable.\
-                    ");
+                        let aborted = ws.manage_with_timeout(
+                            Duration::from_secs(crate::CONFIG.websocket_timeout()),
+                            tcp_stream
+                        ).await;
+                        if aborted {
+                            crate::WARNING!("[WARNING] \
+                                WebSocket session aborted by timeout. In Ohkami, \
+                                WebSocket timeout is set to 3600 seconds (1 hour) \
+                                by default and is configurable by `OHKAMI_WEBSOCKET_TIMEOUT` \
+                                environment variable.\
+                            ");
+                        }
+
+                        crate::DEBUG!("WebSocket session finished");
+                    }
+                    Err(msg) => {
+                        crate::WARNING!("[WARNING] {}", msg);
+                    }
                 }
-
-                crate::DEBUG!("WebSocket session finished");
             }
+        }
+    }
+}
+
+// There has to be some cleaner implementation to apply the conditional trait bounds in this...
+#[cfg(not(feature="ws"))]
+impl<S> Session<S>
+where
+    S: AsyncRead + AsyncWrite + Unpin,
+{
+    pub(crate) fn new(
+        router: Arc<Router>,
+        connection: S,
+        ip: std::net::IpAddr
+    ) -> Self {
+        Self {
+            router,
+            connection,
+            ip
+        }
+    }
+
+    pub(crate) async fn manage(mut self) {
+        #[cold] #[inline(never)]
+        fn panicking(panic: Box<dyn Any + Send>) -> Response {
+            if let Some(msg) = panic.downcast_ref::<String>() {
+                crate::WARNING!("[Panicked]: {msg}");
+            } else if let Some(msg) = panic.downcast_ref::<&str>() {
+                crate::WARNING!("[Panicked]: {msg}");
+            } else {
+                crate::WARNING!("[Panicked]");
+            }
+            crate::Response::InternalServerError()
+        }
+
+        match timeout_in(Duration::from_secs(crate::CONFIG.keepalive_timeout()), async {
+            let mut req = Request::init(self.ip);
+            let mut req = unsafe {Pin::new_unchecked(&mut req)};
+            loop {
+                req.clear();
+                match req.as_mut().read(&mut self.connection).await {
+                    Ok(Some(())) => {
+                        let close = matches!(req.headers.Connection(), Some("close" | "Close"));
+
+                        let res = match catch_unwind(AssertUnwindSafe({
+                            let req = req.as_mut();
+                            || self.router.handle(req.get_mut())
+                        })) {
+                            Ok(future) => future.await,
+                            Err(panic) => panicking(panic),
+                        };
+                        let upgrade = res.send(&mut self.connection).await;
+
+                        if !upgrade.is_none() {break upgrade}
+                        if close {break Upgrade::None}
+                    }
+                    Ok(None) => break Upgrade::None,
+                    Err(res) => {res.send(&mut self.connection).await;},
+                }
+            }
+        }).await {
+            None => crate::WARNING!("[WARNING] \
+                Session timeouted. In Ohkami, Keep-Alive timeout \
+                is set to 42 seconds by default and is configurable \
+                by `OHKAMI_KEEPALIVE_TIMEOUT` environment variable.\
+            "),
+
+            Some(Upgrade::None) => crate::DEBUG!("about to shutdown connection"),
         }
     }
 }
---
+++ ohkami/src/tls/mod.rs
@@ -0,0 +1,52 @@
+use tokio::io::{AsyncRead, AsyncWrite};
+pub struct TlsStream(pub tokio_rustls::server::TlsStream<tokio::net::TcpStream>);
+
+#[cfg(feature="ws")]
+impl crate::session::WebSocketUpgradeable for TlsStream {
+    fn into_websocket_stream(self) -> Result<crate::__rt__::TcpStream, &'static str> {
+        Err("WebSocket connections are not supported over TLS yet")
+    }
+}
+
+impl AsyncRead for TlsStream {
+    fn poll_read(
+        mut self: std::pin::Pin<&mut Self>, 
+        cx: &mut std::task::Context<'_>, 
+        buf: &mut tokio::io::ReadBuf<'_>
+    ) -> std::task::Poll<std::io::Result<()>> {
+        match std::pin::Pin::new(&mut self.0).poll_read(cx, buf) {
+            std::task::Poll::Ready(Err(e)) => {
+                if e.to_string().contains("close_notify") {
+                    std::task::Poll::Ready(Ok(()))
+                } else {
+                    std::task::Poll::Ready(Err(e))
+                }
+            },
+            other => other,
+        }
+    }
+}
+
+impl AsyncWrite for TlsStream {
+    fn poll_write(
+        mut self: std::pin::Pin<&mut Self>, 
+        cx: &mut std::task::Context<'_>, 
+        buf: &[u8]
+    ) -> std::task::Poll<std::io::Result<usize>> {
+        std::pin::Pin::new(&mut self.0).poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        mut self: std::pin::Pin<&mut Self>, 
+        cx: &mut std::task::Context<'_>
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.0).poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        mut self: std::pin::Pin<&mut Self>, 
+        cx: &mut std::task::Context<'_>
+    ) -> std::task::Poll<std::io::Result<()>> {
+        std::pin::Pin::new(&mut self.0).poll_shutdown(cx)
+    }
+}
