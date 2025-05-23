08e99e652e705fe91494d0f81157095ceaa2bf8c
kanarus <kanarus786@gmail.com>
2025-05-24 07:39:50 +0900

    fix(headers): Corretly handle *overset after deletion* (#456)
    * cherry-pick 465f0dee6018eea6610b406c580df07beaf50291
    
    * rm rustc-ice*
    
    * fix pick

--- ohkami/src/header/map.rs
+++ ohkami/src/header/map.rs
@@ -48,15 +48,25 @@ impl<const N: usize, Value> IndexMap<N, Value> {
     }
 
     #[inline(always)]
-    pub(crate) fn iter(&self) -> impl Iterator<Item = &(usize, Value)> {
+    pub(crate) fn iter(&self) -> impl Iterator<Item = (usize, &Value)> {
         self.values.iter()
-            .filter(|(i, _)| *unsafe {self.index.get_unchecked(*i)} != Self::NULL)
+            .enumerate()
+            .filter_map(|(pos, (index, value))| (
+                // `!= Self::NULL` can't correctly handle *over-set after delete*,
+                // we MUST check the held index to be equal to the current position
+                *unsafe {self.index.get_unchecked(*index)} == pos as u8
+            ).then_some((*index, value)))
     }
 
     #[inline(always)]
     pub(crate) fn into_iter(self) -> impl Iterator<Item = (usize, Value)> {
         self.values.into_iter()
-            .filter(move |(i, _)| *unsafe {self.index.get_unchecked(*i)} != Self::NULL)
+            .enumerate()
+            .filter_map(move |(pos, (index, value))| (
+                // `!= Self::NULL` can't correctly handle *over-set after delete*,
+                // we MUST check the held index to be equal to the current position
+                *unsafe {self.index.get_unchecked(index)} == pos as u8
+            ).then_some((index, value)))
     }
 }
 
@@ -80,3 +90,83 @@ const _: () = {
         }
     }
 };
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_index_map_iter_simple() {
+        let mut map = IndexMap::<8, &'static str>::new();
+    
+        unsafe {map.set(0, "a")};
+        unsafe {map.set(1, "b")};
+        unsafe {map.set(2, "c")};
+        unsafe {map.set(3, "d")};
+
+        assert_eq!(
+            map.into_iter().collect::<Vec<_>>(),
+            vec![(0, "a"), (1, "b"), (2, "c"), (3, "d")]
+        );
+    }
+
+    #[test]
+    fn test_index_map_iter_with_delete() {
+        let mut map = IndexMap::<8, &'static str>::new();
+    
+        unsafe {map.set(0, "a")};
+        unsafe {map.set(1, "b")};
+        unsafe {map.set(2, "c")};
+        unsafe {map.set(3, "d")};
+
+        unsafe {map.delete(1)};
+        unsafe {map.delete(3)};
+
+        assert_eq!(
+            map.into_iter().collect::<Vec<_>>(),
+            vec![(0, "a"), (2, "c")]
+        );
+    }
+
+    #[test]
+    fn test_index_map_iter_with_delete_and_other_set() {
+        let mut map = IndexMap::<8, &'static str>::new();
+    
+        unsafe {map.set(0, "a")};
+        unsafe {map.set(1, "b")};
+        unsafe {map.set(2, "c")};
+        unsafe {map.set(3, "d")};
+
+        unsafe {map.delete(1)};
+        unsafe {map.delete(3)};
+
+        unsafe {map.set(4, "e")};
+        unsafe {map.set(5, "f")};
+
+        assert_eq!(
+            map.into_iter().collect::<Vec<_>>(),
+            vec![(0, "a"), (2, "c"), (4, "e"), (5, "f")]
+        );
+    }
+
+    #[test]
+    fn test_index_map_iter_with_delete_and_overset() {
+        let mut map = IndexMap::<8, &'static str>::new();
+    
+        unsafe {map.set(0, "a")};
+        unsafe {map.set(1, "b")};
+        unsafe {map.set(2, "c")};
+        unsafe {map.set(3, "d")};
+
+        unsafe {map.delete(1)};
+        unsafe {map.delete(3)};
+
+        unsafe {map.set(1, "e")};
+        unsafe {map.set(3, "f")};
+
+        assert_eq!(
+            map.into_iter().collect::<Vec<_>>(),
+            vec![(0, "a"), (2, "c"), (1, "e"), (3, "f")]
+        );
+    }
+}
