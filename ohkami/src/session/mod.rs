8ce2222a5d2fc62b611e2acd18bfe5ad58fd8374
kanarus <kanarus786@gmail.com>
2025-05-31 22:08:44 +0900

    WebSocket timeout as WARNING, Keep-Alive timeout as DEBUG

--- ohkami/src/session/mod.rs
+++ ohkami/src/session/mod.rs
@@ -77,7 +77,7 @@ impl<C: Connection> Session<C> {
             match read_result {
                 None => {
                     crate::DEBUG!("\
-                        Session timeouted. In Ohkami, Keep-Alive timeout \
+                        Reached Keep-Alive timeout. In Ohkami, Keep-Alive timeout \
                         is set to 42 seconds by default and is configurable \
                         by `OHKAMI_KEEPALIVE_TIMEOUT` environment variable.\
                     ");
@@ -139,7 +139,7 @@ impl<C: Connection> Session<C> {
                             tcp_stream
                         ).await;
                         if aborted {
-                            crate::DEBUG!("\
+                            crate::WARNING!("\
                                 WebSocket session aborted by timeout. In Ohkami, \
                                 WebSocket timeout is set to 3600 seconds (1 hour) \
                                 by default and is configurable by `OHKAMI_WEBSOCKET_TIMEOUT` \
