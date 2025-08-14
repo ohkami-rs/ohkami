c02d93ddca8b5f55eb84583ff2f7cff65a607000
kanarus <kanarus786@gmail.com>
2025-08-15 01:30:43 +0900

    chore

--- ohkami/src/session/connection.rs
+++ ohkami/src/session/connection.rs
@@ -17,7 +17,7 @@ impl From<anysc_rustls::server::TlsStream<crate::__rt__::TcpStream>> for Connect
     }
 }
 
-#[cfg(feature="rt_tokio")]
+#[cfg(feature="__io_tokio__")]
 const _: () = {
     impl tokio::io::AsyncRead for Connection {
         fn poll_read(
@@ -70,9 +70,9 @@ const _: () = {
     }
 };
 
-#[cfg(feature="rt_smol")]
+#[cfg(feature="__io__futures__")]
 const _: () = {
-    impl smol::io::AsyncRead for Connection {
+    impl futures_util::io::AsyncRead for Connection {
         fn poll_read(
             self: std::pin::Pin<&mut Self>, 
             cx: &mut std::task::Context<'_>, 
@@ -86,60 +86,7 @@ const _: () = {
         }
     }
     
-    impl smol::io::AsyncWrite for Connection {
-        fn poll_write(
-            self: std::pin::Pin<&mut Self>, 
-            cx: &mut std::task::Context<'_>, 
-            buf: &[u8]
-        ) -> std::task::Poll<std::io::Result<usize>> {
-            match std::pin::Pin::into_inner(self) {
-                Self::Tcp(stream) => std::pin::Pin::new(stream).poll_write(cx, buf),
-                #[cfg(feature="tls")]
-                Self::Tls(stream) => std::pin::Pin::new(stream).poll_write(cx, buf),
-            }
-        }
-
-        fn poll_flush(
-            self: std::pin::Pin<&mut Self>, 
-            cx: &mut std::task::Context<'_>
-        ) -> std::task::Poll<std::io::Result<()>> {
-            match std::pin::Pin::into_inner(self) {
-                Self::Tcp(stream) => std::pin::Pin::new(stream).poll_flush(cx),
-                #[cfg(feature="tls")]
-                Self::Tls(stream) => std::pin::Pin::new(stream).poll_flush(cx),
-            }
-        }
-
-        fn poll_close(
-            self: std::pin::Pin<&mut Self>, 
-            cx: &mut std::task::Context<'_>
-        ) -> std::task::Poll<std::io::Result<()>> {
-            match std::pin::Pin::into_inner(self) {
-                Self::Tcp(stream) => std::pin::Pin::new(stream).poll_close(cx),
-                #[cfg(feature="tls")]
-                Self::Tls(stream) => std::pin::Pin::new(stream).poll_close(cx),
-            }
-        }
-    }
-};
-
-#[cfg(feature="rt_glommio")]
-const _: () = {
-    impl futures_util::AsyncRead for Connection {
-        fn poll_read(
-            self: std::pin::Pin<&mut Self>, 
-            cx: &mut std::task::Context<'_>, 
-            buf: &mut [u8]
-        ) -> std::task::Poll<std::io::Result<usize>> {
-            match std::pin::Pin::into_inner(self) {
-                Self::Tcp(stream) => std::pin::Pin::new(stream).poll_read(cx, buf),
-                #[cfg(feature="tls")]
-                Self::Tls(stream) => std::pin::Pin::new(stream).poll_read(cx, buf),
-            }
-        }
-    }
-    
-    impl futures_util::AsyncWrite for Connection {
+    impl futures_util::io::AsyncWrite for Connection {
         fn poll_write(
             self: std::pin::Pin<&mut Self>, 
             cx: &mut std::task::Context<'_>, 
