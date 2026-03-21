2f1bfb738459fe4d7c1c9315ae67e44e382c15f5
kanarus <mail@kanarus.dev>
2026-03-21 18:43:57 +0900

    clippy cfg fix

--- ohkami/src/fang/handler/mod.rs
+++ ohkami/src/fang/handler/mod.rs
@@ -58,15 +58,14 @@ impl Handler {
             openapi_operation,
         }
     }
-}
 
-#[cfg(feature = "openapi")]
-impl Handler {
     /// A utility to create an owned `Handler` instance,
     /// with `openapi_operation` cloned and `proc` replaced to a dummy (meaningless thing),
     /// from a `&Handler`.
     ///
     /// (used in [`crate::router::base::Router::to_dummy_owned_for_openapi`])
+    #[cfg(feature = "openapi")]
+    #[cfg(feature = "__rt__")]
     pub(crate) fn to_dummy_owned_for_openapi(&self) -> Self {
         Self::new(
             |_| Box::pin(async { Response::OK() }),
