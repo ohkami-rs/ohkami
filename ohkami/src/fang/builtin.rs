50d0105557bef3227e25caf67ae3126fa48f52b1
Cln <pgcln88backup@gmail.com>
2026-04-09 23:22:49 +0200

    enhance(fang): Cors allow wildcards in subdomain and port, allow numeric IPs, Enamel::corss_origin_resource_policy typo fixed

--- ohkami/src/fang/builtin.rs
+++ ohkami/src/fang/builtin.rs
@@ -22,30 +22,50 @@ mod timeout;
 pub use timeout::Timeout;
 
 fn validate_origin(origin: &str) -> Result<(), &'static str> {
+    //Adds a check for the first characters being http or https, so it cannot be malformed like foobarhttp://example.org/
+    if !origin.starts_with("http") {
+        return Err("invalid origin: 'http' or 'https' scheme is required at the start of the string.")
+    }
     let Some(("http" | "https", rest)) = origin.split_once("://") else {
-        return Err("invalid origin: 'http' or 'https' scheme is required");
+        return Err("invalid origin: 'http' or 'https' scheme is required.");
     };
+    //Adds a check for the maximum length of a domain
+    if rest.chars().count() > 253 {
+        return Err("invalid origin: maximum length 253 for domain exceeded.")
+    }
     let (host, port) = rest
         .split_once(':')
         .map_or((rest, None), |(h, p)| (h, Some(p)));
-    if port.is_some_and(|p| !p.chars().all(|c| c.is_ascii_digit())) {
-        return Err("invalid origin: port must be a number");
+     if port.is_some_and(|p| p != "*" && p.parse::<u16>().is_err()) {
+        return Err("invalid origin: port must be a number between 0 and 65535 or wildcard '*'.");
     }
-    if !host.starts_with(|c: char| c.is_ascii_alphabetic()) {
-        return Err("invalid origin: host must start with an alphabetic character");
+    if !host.starts_with(|c: char| c.is_ascii_alphanumeric() || c == '*') {
+        return Err("invalid origin: host must start with an alphanumeric character or wildcard '*'.");
     }
     if !host.split('.').all(|part| {
         !part.is_empty()
-            && part
-                .chars()
-                .all(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_'))
+        && part.chars().all(|c| matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '*')
+        && part.chars().count() <= 63)
     }) {
-        if host.contains(['/', '?', '#']) {
+        return if host.contains(['/', '?', '#']) {
             // helpful error message for common mistake
-            return Err("invalid origin: path, query and fragment are not allowed");
+            Err("invalid origin: path, query and fragment are not allowed.")
         } else {
-            return Err("invalid origin: invalid host");
+            Err("invalid origin: invalid host.")
         }
     }
+
+    if host.contains('.') {
+        let Some((subdomain, sld)) = host.split_once('.') else {
+            return Err("invalid origin: invalid host")
+        };
+
+        if sld.split('.').all(|part| part.chars().all(|c| c.is_numeric())) {
+            if subdomain == "*" {
+                return Err("invalid origin: subdomain wildcard not allowed in IP")
+            }
+        }
+    }
+
     Ok(())
 }
